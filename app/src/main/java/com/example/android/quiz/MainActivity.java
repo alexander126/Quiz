11ae58fa936cfec7dc562c0e9ffff4bc1c49bdac
alexander126 <akalapsazov@yahoo.com>
2017-12-14 23:48:15 +0000

    First Commit
    Basic Structure

---
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -0,0 +1,241 @@
+package com.example.android.quiz;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView countLabel;
+    private TextView questionLabel;
+    private Button answerBtn1;
+    private Button answerBtn2;
+    private Button answerBtn3;
+    private Button answerBtn4;
+
+    private String rightAnswer;
+    private int rightAnswerCount = 0;
+    private int quizCount = 1;
+    static final private int QUIZ_COUNT = 5;
+
+    ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+
+    String quizData[][] = {
+            // {"Country", "Right Answer", "Choice1", "Choice2", "Choice3"}
+            {"China", "Beijing", "Jakarta", "Manila", "Stockholm"},
+            {"India", "New Delhi", "Beijing", "Bangkok", "Seoul"},
+            {"Indonesia", "Jakarta", "Manila", "New Delhi", "Kuala Lumpur"},
+            {"Japan", "Tokyo", "Bangkok", "Taipei", "Jakarta"},
+            {"Thailand", "Bangkok", "Berlin", "Havana", "Kingston"},
+            {"Brazil", "Brasilia", "Havana", "Bangkok", "Copenhagen"},
+            {"Canada", "Ottawa", "Bern", "Copenhagen", "Jakarta"},
+            {"Cuba", "Havana", "Bern", "London", "Mexico City"},
+            {"Mexico", "Mexico City", "Ottawa", "Berlin", "Santiago"},
+            {"United States", "Washington D.C.", "San Jose", "Buenos Aires", "Kuala Lumpur"},
+            {"France", "Paris", "Ottawa", "Copenhagen", "Tokyo"},
+            {"Germany", "Berlin", "Copenhagen", "Bangkok", "Santiago"},
+            {"Italy", "Rome", "London", "Paris", "Athens"},
+            {"Spain", "Madrid", "Mexico City", "Jakarta", "Havana"},
+            {"United Kingdom", "London", "Rome", "Paris", "Singapore"}
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        countLabel = (TextView)findViewById(R.id.countLabel);
+        questionLabel = (TextView)findViewById(R.id.questionLabel);
+        answerBtn1 = (Button)findViewById(R.id.answerBtn1);
+        answerBtn2 = (Button)findViewById(R.id.answerBtn2);
+        answerBtn3 = (Button)findViewById(R.id.answerBtn3);
+        answerBtn4 = (Button)findViewById(R.id.answerBtn4);
+
+// Create quizArray from quizData.
+        for (int i = 0; i < quizData.length; i++) {
+            // Prepare array.
+            ArrayList<String> tmpArray = new ArrayList<>();
+            tmpArray.add(quizData[i][0]);  // Country
+            tmpArray.add(quizData[i][1]);  // Right Answer
+            tmpArray.add(quizData[i][2]);  // Choice1
+            tmpArray.add(quizData[i][3]);  // Choice2
+            tmpArray.add(quizData[i][4]);  // Choice3
+
+            // Add tmpArray to quizArray.
+            quizArray.add(tmpArray);
+        }
+
+        showNextQuiz();
+
+    }
+
+    public void showNextQuiz() {
+
+        // Update quizCountLabel.
+        countLabel.setText("Q" + quizCount);
+
+        // Generate random number between 0 and 14 (quizArray's size - 1).
+        Random random = new Random();
+        int randomNum = random.nextInt(quizArray.size());
+
+        // Pick one quiz set.
+        ArrayList<String> quiz = quizArray.get(randomNum);
+
+        // Set question and right answer.
+        // Array format: {"Country", "Right Answer", "Choice1", "Choice2", "Choice3"}
+        questionLabel.setText(quiz.get(0));
+        rightAnswer = quiz.get(1);
+
+        // Remove "Country" from quiz and Shuffle choices.
+        quiz.remove(0);
+        Collections.shuffle(quiz);
+
+        // Set Choices.
+        answerBtn1.setText(quiz.get(0));
+        answerBtn2.setText(quiz.get(1));
+        answerBtn3.setText(quiz.get(2));
+        answerBtn4.setText(quiz.get(3));
+
+        // Remove this quiz from quizArray.
+        quizArray.remove(randomNum);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public void checkAnswer(View view) {
+
+        // Get pushed button.
+        Button answerBtn = (Button) findViewById(view.getId());
+        String btnText = answerBtn.getText().toString();
+
+        String alertTitle;
+
+        if (btnText.equals(rightAnswer)) {
+            // Correct!
+            alertTitle = "Correct!";
+            rightAnswerCount++;
+        } else {
+            // Wrong...
+            alertTitle = "Wrong...";
+        }
+
+        // Create Dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(alertTitle);
+        builder.setMessage("Answer : " + rightAnswer);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (quizCount == QUIZ_COUNT) {
+                    // Show Result.
+                    Intent intent = new Intent(getApplicationContext(), Result.class);
+                    intent.putExtra("RIGHT_ANSWER_COUNT", rightAnswerCount);
+                    startActivity(intent);
+
+                } else {
+                    quizCount++;
+                    showNextQuiz();
+                }
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+}
